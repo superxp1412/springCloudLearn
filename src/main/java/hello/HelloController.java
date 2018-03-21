6038ead5dd8015689396c9bc2e079aba1be13c1e
Alchemist <superxp1412@gmail.com>
2018-03-21 22:11:14 +0800

    test another commit

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -9,7 +9,6 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         System.out.println("test for the commit");
-        System.out.printf("fix the bug test");
         return "Greetings from Spring Boot!";
     }
     
